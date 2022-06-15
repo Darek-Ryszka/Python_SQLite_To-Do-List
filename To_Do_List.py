a4bb2c9e79e6f5559cae02ec71bf6ee090c88113
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-15 09:29:25 +0200

    Add menu loop

---
+++ To_Do_List.py
@@ -0,0 +1,10 @@
+user_choice = -1
+
+while user_choice != 5:
+    print("1. Show tasks")
+    print("2. Add tasks")
+    print("3. Delete tasks")
+    print("4. Save changes")
+    print("5. Exit")
+
+    user_choice = int(input("Choose a number: "))
