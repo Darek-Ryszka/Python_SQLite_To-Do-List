134165d5508fb0bd42afc4081a326733c8517960
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-15 09:36:19 +0200

    Add functions names

--- To_Do_List.py
+++ To_Do_List.py
@@ -2,6 +2,23 @@ import sqlite3
 
 connection = sqlite3.connect("todo.db")
 
+
+def create_table(connection):
+    pass
+
+
+def show_tasks(connection):
+    pass
+
+
+def add_task(connection):
+    pass
+
+
+def delete_task(connection):
+    pass
+
+
 while True:
     print()
     print("1. Show tasks")
@@ -11,3 +28,15 @@ while True:
 
     user_choice = int(input("Choose a number: "))
     print()
+
+    if user_choice == 1:
+        show_tasks(connection)
+
+    if user_choice == 2:
+        add_task(connection)
+
+    if user_choice == 3:
+        delete_task(connection)
+
+    if user_choice == 4:
+        break
